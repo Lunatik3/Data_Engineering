32f6d72bb6d17a22f76b1f6c9fe1ac340b03148f
Lunatik3 <147321002+Lunatik3@users.noreply.github.com>
2023-11-01 18:42:26 +0500

    Update test6.py

--- Task_6/test6.py
+++ Task_6/test6.py
@@ -1,36 +1,41 @@
+import requests
 from bs4 import BeautifulSoup
 import json
 
-str_json = ''
-
-with open('text_6_var_28', encoding='utf-8') as file:
+str_json = ""
+with open("test6.json", encoding="utf-8") as file:
     lines = file.readlines()
     for line in lines:
         str_json += line
 
 data = json.loads(str_json)
-data = data['items']
+data = data['created']
 
 soup = BeautifulSoup("""<table>
     <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
+        <th>id</th>
+        <th>title</th>
+        <th>description</th>
+        <th>price</th>
+        <th>discountPercentage</th> 
+        <th>rating</th>
+        <th>stock</th>
+        <th>brand</th>
+        <th>category</th>           
     </tr>
- </table>""", "html.parser")
+</table>""", features="html.parser")
 
 table = soup.contents[0]
+
 for tick in data:
     tr = soup.new_tag("tr")
-    for key, value in tick.items():
+    for key, val in tick.items():
         td = soup.new_tag("td")
-        td.string = value
+        td.string = str(val)
         tr.append(td)
     table.append(tr)
 
-with open('text_6_result.html', 'w') as result:
+with open("task_6.html", "w", encoding="utf-8") as result:
     result.write(soup.prettify())
-    result.write('\n')
+    result.write("\n")
+
